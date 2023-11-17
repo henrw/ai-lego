1a83000fbfd8aebdb72a8d0d44d903354385aaa5
KristinShuyiHan <shh020@ucsd.edu>
2023-11-17 11:57:29 -0800

    Tests deletion functionality

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,35 +1,44 @@
-// Let's make <Card text='Write the docs' /> draggable!
-
-import React from 'react'
-import { useDrag } from 'react-dnd'
-import { ItemTypes } from './Constants'
-import { useState } from 'react'
+// Card.jsx
+import React from "react";
+import { useDrag } from "react-dnd";
+import { ItemTypes } from "./Constants";
+import { useState } from "react";
 
 /**
  * Your Component
  */
-export default function Card({ isDragging }) {
+export default function Card({ id, handleDelete, isDragging }) {
   const [{ opacity }, dragRef] = useDrag(
     () => ({
       type: ItemTypes.CARD,
-      item: {},
+      item: { id },
       collect: (monitor) => ({
-        opacity: monitor.isDragging() ? 0.5 : 1
-      })
+        opacity: monitor.isDragging() ? 0.5 : 1,
+      }),
     }),
-    []
+    [id]
   );
 
   const [text, setText] = useState("");
 
   return (
-    <div ref={dragRef} style={{ opacity }} className='rounded-lg border-2 p-2 border-black w-full my-2 h-28'>
+    <div
+      ref={dragRef}
+      style={{ opacity }}
+      className="rounded-lg border-2 p-2 border-black w-full my-2 h-28 relative"
+    >
       <textarea
         placeholder="Enter text here"
         value={text}
-        onChange={(event) => { setText(event.target.value); }}
-        className='w-full h-full'
+        onChange={(event) => setText(event.target.value)}
+        className="w-full h-full"
       />
+      <button
+        onClick={() => handleDelete(id)}
+        className="absolute top-0 right-0 m-1 text-lg"
+      >
+        ❌
+      </button>
     </div>
-  )
+  );
 }
--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,29 +1,37 @@
-
-import React, { useState } from 'react';
-import Card from './Card';
+// Column.jsx
+import React, { useState } from "react";
+import Card from "./Card";
 
 const Column = ({ stage }) => {
+  const [cards, setCards] = useState([]);
 
-    const [cards, setCards] = useState([]);
+  const addCard = () => {
+    const newCards = [...cards, ""];
+    setCards(newCards);
+  };
 
-    const addCard = () => {
-        const tmpcards = [...cards];
-        tmpcards.push("")
-        setCards(tmpcards);
-    }
+  const handleDelete = (id) => {
+    const newCards = cards.filter((_, index) => index !== id);
+    setCards(newCards);
+  };
 
-    return (
-        <div className='flex flex-col items-center border-dashed border-black border-r-2 p-4'>
-            <p className='font-bold h-20'>{stage}</p>
-            <div className='card-collector w-full'>
-                {cards.map((_, idx)=>{return <Card key={`${stage}-card-${idx}`} />;})}
-            </div>
-            <button className='text-lg' onClick={addCard}>
-                ➕
-            </button>
-        </div>
-    );
+  return (
+    <div className="flex flex-col items-center border-dashed border-black border-r-2 p-4">
+      <p className="font-bold h-20">{stage}</p>
+      <div className="card-collector w-full">
+        {cards.map((_, idx) => (
+          <Card
+            key={`${stage}-card-${idx}`}
+            id={idx}
+            handleDelete={handleDelete}
+          />
+        ))}
+      </div>
+      <button className="text-lg" onClick={addCard}>
+        ➕
+      </button>
+    </div>
+  );
 };
 
 export default Column;
-
