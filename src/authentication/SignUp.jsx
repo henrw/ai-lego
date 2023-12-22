5b03e44c01136a5da3e009dd4006cbf6a308143d
KristinShuyiHan <shh020@ucsd.edu>
2023-12-22 12:53:28 -0800

    Update SignUp.jsx

--- src/authentication/SignUp.jsx
+++ src/authentication/SignUp.jsx
@@ -1,59 +1,68 @@
-import React, { useCallback } from "react";
-import { withRouter } from "../authentication/withRouter";
-import { auth } from "../firebase";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { Navigate, useNavigate } from "react-router-dom";
-import app from "../firebase";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Form, Alert } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import { useUserAuth } from "./UserAuthContext";
 
-const SignUp = ({ history }) => {
-  const navigate = useNavigate();
+const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+  const [password, setPassword] = useState("");
+  const { signUp } = useUserAuth();
+  let navigate = useNavigate();
 
-  const handleSignUp = useCallback(
-    async (event) => {
-      event.preventDefault();
-      const { email, password } = event.target.elements;
-
-      console.log(email.value, password.value);
-      createUserWithEmailAndPassword(auth, email.value, password.value)
-        .then((userCredential) => {
-          // User created
-          const user = userCredential.user;
-          navigate("/");
-        })
-        .catch((error) => {
-          // Handle errors
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          // ...
-        });
-    },
-    [navigate]
-  );
-
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await signUp(email, password);
+      navigate("/");
+    } catch (err) {
+      setError(err.message);
+    }
+  };
   return (
     <div className="flex h-[calc(100vh-112px)]">
       <div className="m-auto flex flex-col items-center ">
-        <div className="text-xl font-bold m-2">Sign Up</div>
-        <form onSubmit={handleSignUp}>
-          <div className="flex flex-col">
-            <label className="flex flex-row my-1">
-              <div className="mr-auto px-2">Email</div>
-              <input name="email" type="email" placeholder="Email" />
-            </label>
-            <label className="flex flex-row my-1">
-              <div className="mr-auto px-2">Password</div>
-              <input name="password" type="password" placeholder="Password" />
-            </label>
-            <div className="flex justify-center">
-              <button
-                type="submit"
-                className="bg-gray-300 my-2 w-20 p-1 rounded-lg"
-              >
-                Sign Up
-              </button>
-            </div>
+        <div className="text-xl font-bold m-2">Firebase/ React Auth Signup</div>
+
+        {error && <div className="text-red-500">{error}</div>}
+
+        <form onSubmit={handleSubmit} className="flex flex-col">
+          <label className="flex flex-row my-1">
+            <div className="mr-auto px-2">Email</div>
+            <input
+              type="email"
+              placeholder="Email address"
+              onChange={(e) => setEmail(e.target.value)}
+              className="px-2 py-1 border rounded"
+            />
+          </label>
+
+          <label className="flex flex-row my-1">
+            <div className="mr-auto px-2">Password</div>
+            <input
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+              className="px-2 py-1 border rounded"
+            />
+          </label>
+
+          <div className="d-grid gap-2 mt-3">
+            <Button variant="primary" type="submit">
+              Sign up
+            </Button>
           </div>
         </form>
+
+        <div className="text-center mt-3">
+          Already have an account?
+          <Link to="/" className=" text-blue-500">
+            {" "}
+            Log In
+          </Link>
+        </div>
       </div>
     </div>
   );
