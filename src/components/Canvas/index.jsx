adc05f055e5863381682071d3d903b3e1aec7c55
Muzhe Wu <635717823@qq.com>
2024-08-22 20:39:44 -0400

    disable persona feature

--- src/components/Canvas/index.jsx
+++ src/components/Canvas/index.jsx
@@ -351,11 +351,11 @@ const Canvas = () => {
       <p className="fixed bottom-[170px] right-6 font-bold text-lg z-10">Evaluation:</p>
       <ProblemEvaluation isExpanded={isProblemEvaluationExpanded} setIsExpanded={setIsProblemEvaluationExpanded} selectedCardIds={[...selectedCardIds]} number={evaluations.length} cardsData={cardsData} cardId2number={cardId2number} />
       <StageEvaluation isExpanded={isStageEvaluationExpanded} setIsExpanded={setIsStageEvaluationExpanded} selectedCardIds={[...selectedCardIds]} number={evaluations.length} cardsData={cardsData} cardId2number={cardId2number} />
-      <PersonaEvaluation isExpanded={isPersonaEvaluationExpanded} setIsExpanded={setIsPersonaEvaluationExpanded} number={evaluations.length} cardsData={cardsData} cardId2number={cardId2number} selectedPersona={personas.filter(item => item.isSelected === true)} />
+      {/* <PersonaEvaluation isExpanded={isPersonaEvaluationExpanded} setIsExpanded={setIsPersonaEvaluationExpanded} number={evaluations.length} cardsData={cardsData} cardId2number={cardId2number} selectedPersona={personas.filter(item => item.isSelected === true)} /> */}
 
       <MiniMap />
 
-      <div className={`fixed top-20 ${(isProblemEvaluationExpanded || isStageEvaluationExpanded || isPersonaEvaluationExpanded) ? "right-[400px]" : "right-2"} mb-4 ml-4 z-10`}>
+      {/* <div className={`fixed top-20 ${(isProblemEvaluationExpanded || isStageEvaluationExpanded || isPersonaEvaluationExpanded) ? "right-[400px]" : "right-2"} mb-4 ml-4 z-10`}>
         <div className="flex flex-row">
           <p className="font-bold text-lg mr-2">Personas:</p>
           <div className="ml-auto flex flex-row fixed relative">
@@ -417,7 +417,7 @@ const Canvas = () => {
           }
 
         </div>
-      </div>
+      </div> */}
 
       <div className="fixed bottom-0 left-0 mb-4 ml-4 z-10">
         <div className="flex flex-row">
